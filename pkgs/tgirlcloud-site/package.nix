7e4c6c3365eaa26b452200b7595b2e4f7aef9220
isabel <isabel@isabelroses.com>
2025-02-22 09:02:32 +0000

    tgirlcloud-site: init

---
+++ pkgs/tgirlcloud-site/package.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  pnpm,
+  nodejs-slim,
+  stdenvNoCC,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "tgirlcloud-site";
+  version = "0.0.1";
+
+  src = fetchFromGitHub {
+    owner = "tgirlcloud";
+    repo = "site";
+    rev = "meow";
+    hash = "meow";
+  };
+
+  nativeBuildInputs = [
+    pnpm.configHook
+    nodejs-slim
+  ];
+
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-zntHo+O8Hck3YjkPwGaHhuYvGIqGEj7JP8YWwUL4TPk=";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    pnpm run build
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r dist/* $out
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version"
+      "branch=HEAD"
+    ];
+  };
+
+  meta = {
+    description = "The source code tgirl.cloud site";
+    homepage = "https://github.com/tgirlcloud/site";
+    license = lib.licenses.eupl12;
+    maintainers = with lib.maintainers; [ isabelroses ];
+  };
+})
