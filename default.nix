9e8ab6208079922a26cf81d323805ccc0a5665c6
isabel <isabel@isabelroses.com>
2025-05-09 21:50:59 +0100

    refactor: by-name stucture

--- default.nix
+++ default.nix
@@ -8,9 +8,24 @@
   system ? builtins.currentSystem,
 }:
 let
-  packages = lib.packagesFromDirectoryRecursive {
-    directory = ./pkgs;
-    callPackage = lib.callPackageWith (pkgs // packages);
-  };
+  inherit (builtins) readDir;
+  inherit (lib) mapAttrs mergeAttrsList mapAttrsToList;
+
+  baseDirectory = ./pkgs;
+
+  # logic based on
+  # https://github.com/NixOS/nixpkgs/blob/7d7db0123ed366fe21d80ea7fec3a98746770013/pkgs/top-level/by-name-overlay.nix
+  packagesForShard =
+    shard: type:
+    mapAttrs (name: _: baseDirectory + "/${shard}/${name}/package.nix") (
+      readDir (baseDirectory + "/${shard}")
+    );
+
+  packagesFiles = mergeAttrsList (mapAttrsToList packagesForShard (readDir baseDirectory));
+
+  callPackage = lib.callPackageWith (pkgs // packages);
+  callPackage' = file: callPackage file { };
+
+  packages = mapAttrs (_: callPackage') packagesFiles;
 in
 packages
