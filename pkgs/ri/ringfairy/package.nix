8ff0a36c68d75da77265466729706c1639409d52
isabel <isabel@isabelroses.com>
2025-06-09 15:53:38 +0100

    ringfairy: init at 0.1.3-alpha-unstable-2025-05-16

---
+++ pkgs/ri/ringfairy/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  nix-update-script,
+}:
+rustPlatform.buildRustPackage {
+  pname = "ringfairy";
+  version = "0.1.3-alpha-unstable-2025-05-16";
+
+  src = fetchFromGitHub {
+    owner = "k3rs3d";
+    repo = "ringfairy";
+    rev = "b33407e4a61cea9af0cb3ad18cd8bd07914e792a";
+    hash = "sha256-Ft+lfPPHbPd8sLx8F6KFON1PAFBn4OU3D6LYjKa7PK0=";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-rdnTYMdymEqyLU/hG3uoQTT3SOS/SkEOSHJfxMHaPyI=";
+
+  buildInputs = [ openssl ];
+  nativeBuildInputs = [ pkg-config ];
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version"
+      "branch=HEAD"
+    ];
+  };
+
+  meta = {
+    description = "Static webring generator in Rust";
+    homepage = "https://github.com/k3rs3d/ringfairy";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ isabelroses ];
+    mainProgram = "ringfairy";
+  };
+}
