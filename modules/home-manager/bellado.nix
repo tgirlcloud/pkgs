72a757593946c270110cc3f5576edc2bf7fb2c43
isabel <isabel@isabelroses.com>
2025-07-03 15:21:21 +0100

    home-manager/bellado: modernize

--- modules/home-manager/bellado.nix
+++ modules/home-manager/bellado.nix
@@ -9,13 +9,6 @@ let
   inherit (lib) mkEnableOption mkIf optionalAttrs;
 
   cfg = config.programs.bellado;
-
-  aliases = optionalAttrs cfg.enableAliases {
-    bel = "bellado";
-    bell = "bellado -l";
-    bella = "bellado -la";
-    bellc = "bellado -lc";
-  };
 in
 {
   options.programs.bellado = {
@@ -27,12 +20,11 @@ in
   config = mkIf cfg.enable {
     home.packages = [ tgirlpkgs.packages.${pkgs.stdenv.hostPlatform.system}.bellado ];
 
-    programs = {
-      bash.shellAliases = aliases;
-      zsh.shellAliases = aliases;
-      fish.shellAliases = aliases;
-      ion.shellAliases = aliases;
-      nushell.shellAliases = aliases;
+    home.shellAliases = optionalAttrs cfg.enableAliases {
+      bel = "bellado";
+      bell = "bellado -l";
+      bella = "bellado -la";
+      bellc = "bellado -lc";
     };
   };
 }
