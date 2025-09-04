e37520861e648c76ee63ffad2d560bd45326bf92
isabel <isabel@isabelroses.com>
2025-09-04 09:39:29 +0100

    pds-gatekeeper: 0.1.0 -> 0-unstbale-2025-09-03

--- pkgs/pd/pds-gatekeeper/package.nix
+++ pkgs/pd/pds-gatekeeper/package.nix
@@ -2,27 +2,19 @@
   lib,
   rustPlatform,
   fetchFromTangled,
-  nix-update-script,
 }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pds-gatekeeper";
-  version = "0.1.0";
+  version = "0-unstable-2025-09-03";
 
   src = fetchFromTangled {
     owner = "@baileytownsend.dev";
     repo = "pds-gatekeeper";
-    rev = "31b2279dad186a1a2ad0a5c7b3c4cf3b5563d843";
-    hash = "sha256-BOQqNwPR2ZkK25HG0Jh5H+jB1w03eqvKQhgg/UtYg7Y=";
+    rev = "55f5af015ca990aff4d9d30d4a87a9bde27570de";
+    hash = "sha256-d/oWAWJBmBuB+0P3OlxoeOE3UAG2SJ3lBMK7J5Rh5L4=";
   };
 
   cargoHash = "sha256-aaXOfrLftNZzyel+/+HcDYRQOqMpSr8qj1VkRaBwHAE=";
-
-  passthru.updateScript = nix-update-script {
-    extraArgs = [
-      "--version"
-      "branch=HEAD"
-    ];
-  };
 
   meta = {
     description = "microservice that sits on the same server as the PDS to add some of the security that the entryway does";
@@ -31,4 +23,4 @@ rustPlatform.buildRustPackage {
     maintainers = with lib.maintainers; [ isabelroses ];
     mainProgram = "pds_gatekeeper";
   };
-}
+})
