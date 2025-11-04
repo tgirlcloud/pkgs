869d3c9bc149c0b4fb17742ad18a632aca98808f
isabel <isabel@isabelroses.com>
2025-11-04 11:00:58 +0000

    tidaluna: fix build error

--- pkgs/ti/tidaluna/package.nix
+++ pkgs/ti/tidaluna/package.nix
@@ -1,11 +1,11 @@
 {
   tidal-hifi,
-  tideluna-injector,
+  tidaluna-injector,
 }:
 tidal-hifi.overrideAttrs (oa: {
   postInstall = ''
     mv $out/share/tidal-hifi/resources/app.asar $out/share/tidal-hifi/resources/original.asar
     mkdir -p "$out/share/tidal-hifi/resources/app/"
-    cp -R ${tideluna-injector}/* $out/share/tidal-hifi/resources/app/
+    cp -R ${tidaluna-injector}/* $out/share/tidal-hifi/resources/app/
   '';
 })
