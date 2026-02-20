0a3aa597b91bd32c1c058d4b822fbde9f86c346d
isabel <isabel@isabelroses.com>
2026-02-20 14:48:09 +0000

    nixpkgs-prs: 0.5.1-unstable-2025-04-07 -> 0.5.1-unstable-2026-02-19
    Diff: https://github.com/isabelroses/nixpkgs-prs-bot/compare/19ace0ce5a42333fba7a3f14d9ddec48e1677060...69d1b60331c5402836ba0b81c70fef7c84233691

--- pkgs/ni/nixpkgs-prs/package.nix
+++ pkgs/ni/nixpkgs-prs/package.nix
@@ -8,16 +8,16 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "nixpkgs-prs";
-  version = "0.5.1-unstable-2025-04-07";
+  version = "0.5.1-unstable-2026-02-19";
 
   src = fetchFromGitHub {
     owner = "isabelroses";
     repo = "nixpkgs-prs-bot";
-    rev = "19ace0ce5a42333fba7a3f14d9ddec48e1677060";
-    hash = "sha256-KDoJ2UNPOt6vVdNhRnXt5ZS47SyAYUzGcOMIXKgnAno=";
+    rev = "69d1b60331c5402836ba0b81c70fef7c84233691";
+    hash = "sha256-PHM8aqZVFM3qa9rARGy0RQBynwH4dW90LnMwPqpCSdE=";
   };
 
-  cargoHash = "sha256-PKC9I9T7mYIiDANVXkJRX4ErxJgHXJwl4An3ulx5zVo=";
+  cargoHash = "sha256-58kDUaGD+MP14Y+yXXXJ6zUs5E/vV3dtWkxPcE72o+M=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
