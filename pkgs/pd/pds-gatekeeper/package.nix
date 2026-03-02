256f28570157348e48c5bba4b176d242276daccd
isabel <isabel@isabelroses.com>
2026-03-02 15:26:29 +0000

    pds-gatekeeper: 0-unstable-2025-10-07 -> 0-unstable-2026-01-05

--- pkgs/pd/pds-gatekeeper/package.nix
+++ pkgs/pd/pds-gatekeeper/package.nix
@@ -2,19 +2,24 @@
   lib,
   rustPlatform,
   fetchFromTangled,
+  pkg-config,
+  openssl,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pds-gatekeeper";
-  version = "0-unstable-2025-10-07";
+  version = "0-unstable-2026-01-05";
 
   src = fetchFromTangled {
     owner = "@baileytownsend.dev";
     repo = "pds-gatekeeper";
-    rev = "3d3b821be3a57544b67024353c43ba7f391a6ec1";
-    hash = "sha256-JdhPDpEXzy6CovNGbIMQzzmRtuJoW5LvydpeDNFFpSs=";
+    rev = "ca02ceff4e2febe62fc78a7781f15136d4b4a714";
+    hash = "sha256-bqy/IbPyjgVurIghyyqIY7Hu/0oIb+wXbK2fRdW3YuM=";
   };
 
-  cargoHash = "sha256-ht3WEmuVzup/PKUIe++8F+TKRCh+NskDQfiKsMkYuH8=";
+  cargoHash = "sha256-phNpzZ6V37YA5N927iMpoKRp0fVdgzjo0odZxuHuOrE=";
+
+  buildInputs = [ openssl ];
+  nativeBuildInputs = [ pkg-config ];
 
   meta = {
     description = "microservice that sits on the same server as the PDS to add some of the security that the entryway does";
